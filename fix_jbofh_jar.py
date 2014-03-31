dbc9989b1fcc9d2518420a6875258ebc28a9a2e9
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2014-03-31 11:10:08 +0200

    Added tmp-dirs from fix_jbofh_jar to .gitignore
    Docstrings and comments in fix_jbofh_jar.py. Also changed exception type thrown
    from the script, and the args for the 'jar' command. As is was, the 'jar'
    command would print the new jar file to stdout... Not ideal.

--- fix_jbofh_jar.py
+++ fix_jbofh_jar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-
-# Copyright 2003 University of Oslo, Norway
+# -*- coding: utf-8 -*-
+#
+# Copyright 2003, 2014 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -18,38 +18,80 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+""" Script to replace the config and CA-certs in jBofh.
 
+This script can replace one of (or both) the following files in jBofh.jar:
+
+    - jbofh.properties   Properties file
+    - cacert.pem         CA-certificate chain(s)
+
+Both of these files are packaged with the jBofh jar file as part of the build.
+Running this script will create a file 'jbofh_new.jar', which will contain the
+new files, specified as arguments.
+
+This utility depends on:
+    - unzip
+    - jar
+
+"""
 import getopt
 import os
 import shutil
 import sys
 import time
 
+
 def fix_file(jar_file, cert_file, property_file):
-    zip = 'zip'
+    """ Copy ca-cert file and/or prop-file into the jar file.
+
+    @type jar_file: str
+    @param jar_file: filename of the jar-file
+
+    @type cert_file: str or NoneType
+    @param cert_file:
+        filename of the ca-cert file (or None if we're not (re)placing a
+        cert-file.
+
+    @type property_file: str or NoneType
+    @param property_file:
+        filename of the .properties file (or None if we're not (re)placing a
+        property file.
+
+    """
     jar = 'jar'
     unzip = 'unzip'
     new_file = 'jbofh_new.jar'
     tmp_dir = 'tmp_%s' % time.time()
+
+    # Make tmp work dir
     os.mkdir(tmp_dir)
+
+    # Unzip jar to tmp_dir
     cmd = [unzip, '-d', tmp_dir, jar_file]
     ret = os.spawnvp(os.P_WAIT, unzip, cmd)
     if ret != 0:
-        raise IOError, "Error running %s" % unzip
+        raise SystemExit("Error running: '%s'" % unzip)
+
+    # Copy (replacement) files to tmp_dir
     if property_file is not None:
         shutil.copyfile(property_file, tmp_dir + '/jbofh.properties')
     if cert_file is not None:
         shutil.copyfile(cert_file, tmp_dir + '/cacert.pem')
+
+    # Repackage tmp_dir to jar
     os.chdir(tmp_dir)
-    cmd = [jar, '-c', '-f', '../%s' % new_file, '.']
+    cmd = [jar, 'cf', '../%s' % new_file, '.']
     ret = os.spawnvp(os.P_WAIT, jar, cmd)
     if ret != 0:
-        raise IOError, "Error running %s" % jar
+        raise SystemExit("Error running: '%s'" % jar)
+
     print "New file: %s" % new_file
     os.chdir('..')
     shutil.rmtree(tmp_dir)
 
+
 def usage():
+    """ Print usage string. """
     print """Usage: fix_jbofh_jar.py [-c cert_file | -p property_file] jar_file
 
 This utility is for people who want to update the JBofh.jar file
@@ -57,7 +99,9 @@ without running ant.  It can replace the cacert.pem and
 jbofh.properties files.
 """
 
+
 def main():
+    """ Main. """
     opts, args = getopt.getopt(sys.argv[1:], 'c:p:h',
                                ['cert-file', 'property-file', 'help'])
 
@@ -77,9 +121,9 @@ def main():
     if not jar_file.endswith(".jar"):
         usage()
         sys.exit()
+
+    # Do the actual work
     fix_file(jar_file, cert_file, property_file)
 
 if __name__ == '__main__':
     main()
-
-# arch-tag: ca29e86c-4b9c-47ae-b7ab-7a972042238b

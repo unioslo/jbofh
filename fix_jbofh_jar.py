00ec14aa6e32784408f49f5175af2e5980b860c4
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-10-23 14:16:30 +0200

    Add support for adding log config in jar-archive
    Adds support for patching the log4j.properties and
    log4j_normal.properties into a jbofh jar-file.

--- fix_jbofh_jar.py
+++ fix_jbofh_jar.py
@@ -21,18 +21,34 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 """ Script to replace the config and CA-certs in jBofh.
 
-This script can replace one of (or both) the following files in jBofh.jar:
+Files
+-----
+This script can replace one or more of the following files in a jBofh.jar
+archive:
 
-    - jbofh.properties   Properties file
-    - cacert.pem         CA-certificate chain(s)
+jbofh.properties
+    JBofh configuration file
 
-Both of these files are packaged with the jBofh jar file as part of the build.
+cacert.pem
+    Custom CA-certificate chain(s) for use with the JBofh setting
+    ``InternalTrustManager.enable=true``
+
+log4j.properties
+    Debug log configuration (loaded with the ``-d`` flag in JBofh)
+
+log4j_normal.properties
+    Default log configuration
+
+All these files are packaged with the jBofh jar file as part of the build.
 Running this script will create a file 'jbofh_new.jar', which will contain the
 new files, specified as arguments.
 
+Dependencies
+------------
 This utility depends on:
-    - unzip
-    - jar
+
+ - unzip
+ - jar
 """
 import argparse
 import logging
@@ -54,14 +70,24 @@ DEFAULT_LOG_LEVEL = logging.INFO
 
 JAR_CMD = 'jar'
 
+# Files that can be replaced - and the arguments to do so.
+KNOWN_FILES = (
+    ('cacert.pem', 'cacerts', ('-c', '--cert-file')),
+    ('jbofh.properties', 'settings', ('-p', '--property-file')),
+    ('log4j.properties', 'logconf_debug', ('--log-config-debug',)),
+    ('log4j_normal.properties', 'logconf_normal', ('--log-config-normal',)),
+)
+
 
 class TempDir(object):
+    """ A simple tempfile.mkdtemp context. """
 
     def __init__(self, suffix='jbofh-workdir'):
         self.suffix = suffix
         self.path = None
 
     def copy(self, filename, destname):
+        """ Copy ``filename`` into the tempdir as ``destname``. """
         dest = os.path.join(self.path, destname)
         logger.debug('copying file=%r to %r', filename, dest)
         shutil.copyfile(filename, dest)
@@ -78,6 +104,11 @@ class TempDir(object):
 
 
 def run_cmd(cmd):
+    """
+    Run a command using subprocess.Popen
+
+    :type cmd: list
+    """
     pretty = ' '.join(cmd_quote(arg) for arg in cmd)
     logger.debug('running: %s', pretty)
 
@@ -97,25 +128,22 @@ def run_cmd(cmd):
                            (cmd, proc.returncode))
 
 
-def patch_jar(filename, cert_file=None, property_file=None):
+def patch_jar(filename, filemap):
     """
     Copy ca-cert file and/or prop-file into a jar file.
 
     :type filename: str
     :param filename: the jar-file to patch
 
-    :type cert_file: str or NoneType
-    :param cert_file:
-        filename of the ca-cert file (or None if we're not (re)placing a
-        cert-file.
+    :type filemap: dict
+    :param filemap:
+        A mapping of files to patch into the jar file. E.g.:
 
-    :type property_file: str or NoneType
-    :param property_file:
-        filename of the .properties file (or None if we're not (re)placing a
-        property file.
+        {'jbofh.properties': '/path/to/file.properties',
+         'cacert.pem': '/path/to/certs.pem'}
     """
-    if (not cert_file) and (not property_file):
-        raise SystemExit("Nothing to do.")
+    if not filemap:
+        raise ValueError("No files given - nothing to do!")
 
     update_cmd = [JAR_CMD, 'uf', filename]
 
@@ -123,12 +151,9 @@ def patch_jar(filename, cert_file=None, property_file=None):
         # Copy (replacement) files to workdir
         # We need to recreate the same directory and file name structure as we
         # want the files to have in the jar archive.
-        if property_file is not None:
-            workdir.copy(property_file, 'jbofh.properties')
-            update_cmd.extend(['-C', workdir.path, 'jbofh.properties'])
-        if cert_file is not None:
-            workdir.copy(cert_file, 'cacert.pem')
-            update_cmd.extend(['-C', workdir.path, 'cacert.pem'])
+        for name in filemap:
+            workdir.copy(filemap[name], name)
+            update_cmd.extend(['-C', workdir.path, name])
 
         # Update dest with the given files
         run_cmd(update_cmd)
@@ -147,7 +172,7 @@ def filename_type(filename):
     if not os.path.exists(filename):
         raise ValueError('No file %r' % (filename, ))
     if not os.path.isfile(filename):
-        raise ValueError('Path %r is not a regular file' % (filename, ))
+        raise ValueError('File %r is not a regular file' % (filename, ))
     return filename
 
 
@@ -157,21 +182,26 @@ def main(inargs=None):
         description=DESCRIPTION,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument(
-        '-c', '--cert-file',
-        dest='cacerts',
-        type=filename_type,
-        help='Replace cacert.pem in the JAR-file with %(metavar)s',
-        metavar='<file>',
-    )
-    parser.add_argument(
-        '-p', '--property-file',
-        dest='properties',
-        type=filename_type,
-        help='Replace jbofh.properties in the JAR-file with %(metavar)s',
-        metavar='<file>',
-    )
-    out_file = parser.add_mutually_exclusive_group()
+
+    # Patchable files
+    files_args = parser.add_argument_group(
+        'files',
+        'Files to patch into archive.')
+    for filename, dest, args in KNOWN_FILES:
+        kwargs = {
+            'dest': dest,
+            'type': filename_type,
+            'help': ('Replace {} in the JAR-file '
+                     'with %(metavar)s').format(filename),
+            'metavar': '<file>',
+        }
+        files_args.add_argument(*args, **kwargs)
+
+    # In/out
+    io_args = parser.add_argument_group(
+        'archives',
+        'Source and destination archives')
+    out_file = io_args.add_mutually_exclusive_group()
     out_file.add_argument(
         '-o', '--output',
         dest='destination',
@@ -186,7 +216,7 @@ def main(inargs=None):
         const=None,
         help='Update source JAR-file in-place',
     )
-    parser.add_argument(
+    io_args.add_argument(
         'source',
         type=filename_type,
         help='Input JAR-file',
@@ -194,22 +224,27 @@ def main(inargs=None):
 
     args = parser.parse_args(inargs)
 
-    logger.info('Patching jar=%r with cacerts=%r, properties=%r',
-                args.source, args.cacerts, args.properties)
-
+    # Select jar-file
     if args.destination and args.destination != args.source:
-        logger.debug('copying %r to %r', args.source, args.destination)
+        logger.info('Creating new archive %r from %r',
+                    args.destination, args.source)
         shutil.copyfile(args.source, args.destination)
         filename = args.destination
     else:
-        logger.warning('Updating %r in place!', args.source)
+        logger.warning('Updating archive %r in place!', args.source)
         filename = args.source
 
-    patch_jar(filename=filename,
-              cert_file=args.cacerts,
-              property_file=args.properties)
+    # Select files to patch
+    filemap = {}
+    for name, attr, _ in KNOWN_FILES:
+        value = getattr(args, attr)
+        if not value:
+            continue
+        filemap[name] = value
+        logger.info('Patching archive with %s=%r', name, value)
 
-    logger.info("Archive written to %r", args.destination)
+    patch_jar(filename, filemap)
+    logger.info("Archive %r updated", filename)
 
 
 if __name__ == '__main__':
